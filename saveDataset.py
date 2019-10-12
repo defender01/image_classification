5e0379b374eb1a566f39b5f91f6826642811ab80
defendet01 <r.r.rahman.r.r@gmail.com>
2019-10-13 04:20:28 +0600

    completed

--- saveDataset.py
+++ saveDataset.py
@@ -7,11 +7,6 @@ def save_data(data):
     print('here1')
     print(data['train_images'].shape)
 	
-    np.savetxt('train_images.csv', data['train_images'], delimiter=',', fmt='%f')
-    np.savetxt('train_labels.csv', data['train_labels'], delimiter=',', fmt='%f')
-    np.savetxt('test_images.csv', data['test_images'], delimiter=',', fmt='%f')
-    np.savetxt('test_labels.csv', data['test_labels'], delimiter=',', fmt='%f')
-	
     np.save('train_images.npy', data['train_images'])
     np.save('train_labels.npy', data['train_labels'])
     np.save('test_images.npy', data['test_images'])
